c3760020c74b3fc5109a9d59d30e3454ccc649d5
WasathTheekshana <wasath.vt@gmail.com>
2024-08-16 14:40:21 +0530

    refactor: Static metadata

---
+++ src/data/metadata/static.ts
@@ -0,0 +1,9 @@
+import type { Metadata } from "next";
+
+export const landingPageMetadata: Metadata = {
+  title: "Disowe Labs",
+  description:
+    "Disowe Labs is a software development company that specializes in building web applications and websites. Not only do we build software, but we also provide digital marketing as well.",
+  keywords:
+    "disowe labs, disowe, disowe software company, disowe digital marketing, disowe sri lanka, disowe software development, disowe web development, disowe disign"
+};
