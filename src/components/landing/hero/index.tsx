67060e51bb23934a32a6e19114fa4c9601c77ec4
WasathTheekshana <wasath.vt@gmail.com>
2024-08-19 17:03:46 +0530

    style: Auto prettier

--- src/components/landing/hero/index.tsx
+++ src/components/landing/hero/index.tsx
@@ -5,7 +5,8 @@ import Label from "./label";
 const content = {
   labelText: "Updating with the latest",
   heading: "Bring your dream software into Reality.",
-  paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  paragraph:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   highlights: ["Reality."]
 };
 
@@ -14,9 +15,18 @@ const Hero = () => {
     <div className="bg-card rounded-3xl my-10">
       <Label text={content.labelText} />
       <div className="flex flex-col md:flex-row">
-        <Details heading={content.heading} paragraph={content.paragraph} highlights={content.highlights} />
+        <Details
+          heading={content.heading}
+          paragraph={content.paragraph}
+          highlights={content.highlights}
+        />
         <div className="flex justify-end">
-          <Image src="/images/hero.svg" alt="Hero Image" width={500} height={500} />
+          <Image
+            src="/images/hero.svg"
+            alt="Hero Image"
+            width={500}
+            height={500}
+          />
         </div>
       </div>
     </div>
